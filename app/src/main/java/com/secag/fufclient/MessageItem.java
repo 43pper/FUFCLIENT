94272e807352bb5867f85cd0e30123363414bb87
metraakaldap <metraakladap228@gmail.com>
2022-07-05 23:45:25 +0300

    Chat 1 try

---
+++ app/src/main/java/com/secag/fufclient/MessageItem.java
@@ -0,0 +1,14 @@
+package com.secag.fufclient;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MessageItem extends LinearLayout {
+
+    public MessageItem(Context context) {
+        super(context);
+    }
+
+
+}
